523aa0c54cfde81e5e909c53a4893709573af947
StamKaly <stamkaly@gmail.com>
2018-07-14 22:24:51 +0300

    swapteams' check should be above swap's one

--- flightClub.py
+++ flightClub.py
@@ -146,6 +146,8 @@ class FlightClub(AltitudeMod):
                     self.commands.multiple_whispers(player.nickname,
                                                     ["Wrong structure of function.",
                                                      'Please use it like this: ".move <nickname> <team>"'])
+            elif message == ".swapteams":
+                self.swap_teams()
             elif message.startswith(".swap"):
                 parts = message.split()[1:]
                 if len(parts) == 2:
@@ -154,8 +156,6 @@ class FlightClub(AltitudeMod):
                     self.commands.multiple_whispers(player.nickname,
                                                     ["Wrong structure of function.",
                                                      'Please use it like this: ".swap <nickname 1> <nickname 2>"'])
-            elif message == ".swapteams":
-                self.swap_teams()
             elif message == ".clear":
                 self.teams = [[], []]
                 if self.mode == "tourny":
